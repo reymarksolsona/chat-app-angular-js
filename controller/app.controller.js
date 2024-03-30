ab7abbcc957e79ed121989688651fb4bdf058d73
Rey Mark Solsona <rsolsona@gtreasury.com>
2024-03-30 16:08:33 +0800

    Added logic for prompting name before entering chat

--- controller/app.controller.js
+++ controller/app.controller.js
@@ -1,4 +1,24 @@
 angular.module('app.controller', [])
 .controller('ChatController', function($scope, $window) {
+    var chat = this;
+    chat.userName = $window.localStorage.getItem('userName_') || "";
+    chat.tempUserName = "";
+    chat.namePrompted = false;
+    chat.joined = false;
 
+    chat.enterName = function() {
+        if (!chat.tempUserName.trim()) {
+            alert("Please enter a valid name.");
+            return;
+        }
+        chat.userName = chat.tempUserName;
+        $window.localStorage.setItem('userName_', chat.userName);
+        chat.tempUserName = "";
+        chat.namePrompted = false;
+        chat.joinChat();
+    };
+
+    chat.joinChat = function() {
+        chat.joined = true;
+    };
 });
